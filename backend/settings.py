a85e8886ea55d860e8a5d2e3176e734242022273
Craig Gardner <36039282+Craigpgardner@users.noreply.github.com>
2024-11-19 12:24:19 -0500

    Update settings.py
    Changed chat description

--- backend/settings.py
+++ backend/settings.py
@@ -45,7 +45,7 @@ class _UiSettings(BaseSettings):
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "I'm here to help"
-    chat_description: str = "I am OphAI, I am here to assist with all things Ophthalmology! Got brainfog? Forgot where to put the 1+ cell? Just ask me and ill assist you!"
+    chat_description: str = "I am EyeAI, I am here to assist with all things Ophthalmology! 
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
